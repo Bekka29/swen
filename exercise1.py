eae5d3f53a9fb74cb7ec006eadddadf3529681f5
Bekka29 <115698154+Bekka29@users.noreply.github.com>
2022-10-21 20:44:28 +0000

    added exercise examples

---
+++ exercise1.py
@@ -0,0 +1,12 @@
+#Program to print out greetings
+print("Hello world")
+
+#defining a greeting function
+def greetings(name):
+  print("You are most welcome to this course, ",name)
+
+#declaring the variable name
+name="rebekah"
+
+#calling the defined function
+greetings(name)
---
+++ exercise2.py
@@ -0,0 +1,11 @@
+#program to calculate the force of an object
+def force(mass,acceleration):
+  force=mass*acceleration
+  print(force)
+
+#declaring mass and acceleration
+m=10
+a=5
+
+#finding the force
+force(m,a)
